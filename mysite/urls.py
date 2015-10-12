6f325f0490aad9f7e5d250f9ced0f7587263936b
Dan Kaplan <dank@box.com>
2015-10-12 16:06:31 -0700

    removed polls app

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^box/', include('box.urls', namespace="box"))
 ]
