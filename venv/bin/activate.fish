d0881517197fc7124cac21558bc578d917b682a7
Dan Kaplan <dank@box.com>
2015-10-12 14:31:41 -0700

    trying to get rid of venv directory

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/danielkaplan/dev/mysite/venv"
+set -gx VIRTUAL_ENV "/Users/danielkaplan/dev/BoxApps/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
